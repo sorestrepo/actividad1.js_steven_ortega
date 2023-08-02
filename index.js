1a269d8f07584baef1a32edab9827064bc8b604c
sorestrepo <so3049674@gmail.com>
2023-08-01 19:57:03 -0500

    actividad1.js steven ortega

---
+++ index.js
@@ -0,0 +1,6 @@
+let firtsname = "steven"
+lastname = "Ortega Colorado"
+yearbith = 1999,
+city = " medellin";
+let oldyear = 24
+console.log ("hola, mi nombre es: " + firtsname + " " + lastname + ", vivo en " + city + " y tengo " + oldyear + " años ");
